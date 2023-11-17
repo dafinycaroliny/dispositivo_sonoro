04780a178621a7a248831b4d76f1c51892feee7b
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2023-11-17 11:23:10 -0300

    ok

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ function tocaSomPom(idElementoAudio){
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAllok('.tecla');
 
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+    const teclas = listaDeTeclas[contador]; //guarda o contador da lista
+    const efeito = teclas.classList [1]; //chama o 2°individual do som
+    const idAudio = `#som_${efeito}`; //utiliza a junção template string ${}
+    teclas.
+}
+
 let contador = 0;
 while(contador< listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classList[1];
