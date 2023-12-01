b829f11d87bc1ff7df65f08469107c07ec0625b9
dafinycaroliny <105360433+dafinycaroliny@users.noreply.github.com>
2023-12-01 11:31:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Declaração/criação da função 
-function tocaSomPom(idElementoAudio){
+function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
@@ -8,9 +8,10 @@ const listaDeTeclas = document.querySelectorAllok('.tecla');
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++){
     const teclas = listaDeTeclas[contador]; //guarda o contador da lista
-    const efeito = teclas.classList [1]; //chama o 2°individual do som
-    const idAudio = `#som_${efeito}`; //utiliza a junção template string ${}
-    teclas.
+    const efeito = teclas.classList [1];   //chama o 2°individual do som
+    const idAudio = `#som_${efeito}`;     //utiliza a junção template string ${}
+    teclas.onclick=function(){           //função anônima auxiliar para chamar toca som
+        tocaSom(idAudio);               //chamada da função tocaSom com parâmetro idAudio
 }
 
 let contador = 0;
@@ -20,6 +21,7 @@ while(contador< listaDeTeclas.length){
     listaDeTeclas[contador].onclick = function(){
         tocaSom(idAudio);
     }
+}
     contador = contador+1;
     console.log(contador);
 }
